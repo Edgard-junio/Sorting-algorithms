9f8ad9a1393644e1c0cea7ea837a0827410b5ecc
Leonardo Alexandre da Silva Ferreira <94490826+LeonardoAleee@users.noreply.github.com>
2024-05-18 11:55:24 -0300

    Update insertionSort.cpp

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -161,7 +161,7 @@ void LinkedList::medetempo(int iSize)
     int iCount = 0;
     while (iCount < iSize)
     {
-        std::cout << "teste " << iCount << std::endl;
+        std::cout << "Teste " << iCount << std::endl;
         Node* node1 = nullptr;
         randomList(node1, 10000); // Função que cria nós aleatórios
         Node* node2 = duplicateList(node1);
@@ -171,14 +171,14 @@ void LinkedList::medetempo(int iSize)
         auto timeStop = std::chrono::high_resolution_clock::now(); // Medindo tempo de execução
 
         auto timeDuration = std::chrono::duration_cast<std::chrono::nanoseconds>(timeStop - timeStart);
-        std::cout << "tempo de compilação não otimizada   = " << timeDuration.count() << std::endl;
+        std::cout << "Tempo de compilação não otimizada   = " << timeDuration.count() << std::endl;
         
         timeStart = std::chrono::high_resolution_clock::now();
         optimizedInsertionSort(node2); // Executa o insertionsort otimizado
         timeStop = std::chrono::high_resolution_clock::now(); // Medindo tempo de execução
 
         timeDuration = std::chrono::duration_cast<std::chrono::nanoseconds>(timeStop - timeStart);
-        std::cout << "tempo de compilação otimizada   = " << timeDuration.count() << std::endl;
+        std::cout << "Tempo de compilação otimizada   = " << timeDuration.count() << std::endl;
         std::cout << std::endl;
         iCount++;
     }
