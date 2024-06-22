906f4108f32d41ed0469b4e937315bca4ef3591c
Leonardo Alexandre da Silva Ferreira <94490826+LeonardoAleee@users.noreply.github.com>
2024-06-22 18:52:35 -0300

    Comentários do arquivo selectionSort.cpp

--- SelectionSort/selectionSort.cpp
+++ SelectionSort/selectionSort.cpp
@@ -12,6 +12,7 @@ using std::chrono::high_resolution_clock;
 using std::chrono::duration_cast;
 using std::chrono::nanoseconds;
 
+// Método do Selection Sort não otimizado
 template<typename T>
 void SelectionSort::selectionSort(Node<T>* head)
 {
@@ -19,10 +20,11 @@ void SelectionSort::selectionSort(Node<T>* head)
     Node<T> *innerLoop = nullptr;
     Node<T>* minNode = nullptr;
 
-    for (outerLoop = head; outerLoop -> ptrNext != nullptr; outerLoop = outerLoop -> ptrNext)
+    for (outerLoop = head; outerLoop -> ptrNext != nullptr; outerLoop = outerLoop -> ptrNext) // Percorre a lista até o penúltimo elemento
     {
         minNode = outerLoop;
 
+        // Procura o nó com o menor valor a partir do próximo nó ao nó atual
         for (innerLoop = outerLoop -> ptrNext; innerLoop != nullptr; innerLoop = innerLoop -> ptrNext)
         {
             if (innerLoop -> data < minNode -> data)
@@ -31,10 +33,11 @@ void SelectionSort::selectionSort(Node<T>* head)
             }
         }
 
-        LinkedList::swapValue(outerLoop, minNode); 
+        LinkedList::swapValue(outerLoop, minNode); // Troca os valores dos nós
     }
 }
 
+// Implementação do Selection Sort otimizado
 template<typename T>
 void SelectionSort::optimizedSelectionSort(Node<T>* head)
 {
@@ -42,11 +45,12 @@ void SelectionSort::optimizedSelectionSort(Node<T>* head)
 
     Node<T>* current = head;
 
-    while (current != nullptr)
+    while (current != nullptr) // Percorre a lista até o último nó
     {
         Node<T>* minNode = current;
         Node<T>* nextNode = current -> ptrNext;
 
+        // Procura o nó com o menor valor a partir do próximo nó ao nó atual
         while (nextNode != nullptr)
         {
             if (nextNode -> data < minNode -> data)
@@ -56,6 +60,7 @@ void SelectionSort::optimizedSelectionSort(Node<T>* head)
             nextNode = nextNode->ptrNext;
         }
 
+        // Se o nó mínimo não for o nó atual, troca os valores dos nós
         if (minNode != current)
         {
             LinkedList::swapValue(current, minNode); 
@@ -65,6 +70,7 @@ void SelectionSort::optimizedSelectionSort(Node<T>* head)
     }
 }
 
+// Função para medir o tempo dos dois métodos de Selection Sort
 void SelectionSort::medetempoSelectionSort(int iSize)
 {
     int iCount = 0;
@@ -72,9 +78,10 @@ void SelectionSort::medetempoSelectionSort(int iSize)
     {
         cout << "Teste " << iCount << endl;
         Node<int>* node1 = nullptr;
-        LinkedList::randomList(&node1, 10000);
-        Node<int>* node2 = LinkedList::duplicateList(node1);
+        LinkedList::randomList(&node1, 10000); // Gera uma lista aleatória
+        Node<int>* node2 = LinkedList::duplicateList(node1); // Duplica a lista
 
+        // Mede o tempo da versão não otimizada
         auto timeStart = high_resolution_clock::now();
         selectionSort(node1);
         auto timeStop = high_resolution_clock::now();
@@ -82,6 +89,7 @@ void SelectionSort::medetempoSelectionSort(int iSize)
         auto timeDuration = duration_cast<nanoseconds>(timeStop - timeStart);
         cout << "Tempo de compila��o n�o otimizada = " << timeDuration.count() << endl;
 
+        // Mede o tempo da versão otimizada
         timeStart = high_resolution_clock::now();
         optimizedSelectionSort(node2);
         timeStop = high_resolution_clock::now();
